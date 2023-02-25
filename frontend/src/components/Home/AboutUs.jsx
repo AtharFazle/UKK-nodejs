00c644e5a6dab51fb10d25d59ebf7fced0815412
atharfaz <101614789+atharfaz@users.noreply.github.com>
2023-02-25 21:46:22 +0700

    2.landing page guest proses

--- frontend/src/components/Home/AboutUs.jsx
+++ frontend/src/components/Home/AboutUs.jsx
@@ -3,16 +3,40 @@ import AboutUsImg from "../../assets/AboutUsImg.jpg";
 
 const AboutUs = () => {
   return (
-    <div class="flex flex-wrap mr-24">
-      <div class="w-1/2 mr-auto h-12 flex flex-justify-center items-center">
-        <h1 className="">Hotel Athar asawds</h1>
+    <div class="flex flex-wrap mr-24 ">
+      {/* 1). */}
+      {/* Our way is being */}
+      <div class="w-1/2 mr-auto h-12 flex ">
+        <div className="m-12 mt-36 text-left   md:text-left lg:text-left sm:text-center">
+          <h1 className="font-extrabold text-4xl text-slate-600 ">Our Way is Being</h1>
+          <h2 className="mt-6 font-normal">At Radisson Hotel Group we strive to be the first choice in the mind of guests, owners and talent. In our journey to achieve this, we practice strong beliefs and actions that respect the diversity of people, the community, ethics and the planet.
+          </h2>
+        </div>
       </div>
-      <div class="w-1/2 ml-auto h-12 mt-24">
+      {/* Image */}
+      <div class="w-1/2 ml-auto h-12 mt-24 hidden sm:block">
         <img
           src={AboutUsImg}
-          className="object-fill rounded-lg hidden md:object-scale-down sm:block"
+          className="object-fill rounded-lg md:object-scale-down "
           alt=""
         />
+      </div>
+      {/* 2). */}
+      {/* Image */}
+      <div class="w-1/2 ml-auto h-12 mt-24 hidden sm:block">
+        <img
+          src={AboutUsImg}
+          className="object-fill rounded-lg md:object-scale-down "
+          alt=""
+        />
+      </div>
+      {/* Our way is being */}
+      <div class="w-1/2 mr-auto mt-36 h-12 flex ">
+        <div className="m-12 mt-36 text-left   md:text-left lg:text-left sm:text-center">
+          <h1 className="font-extrabold text-4xl text-slate-600 ">Our Way is Being</h1>
+          <h2 className="mt-6 font-normal">At Radisson Hotel Group we strive to be the first choice in the mind of guests, owners and talent. In our journey to achieve this, we practice strong beliefs and actions that respect the diversity of people, the community, ethics and the planet.
+          </h2>
+        </div>
       </div>
     </div>
   );
--- frontend/src/pages/guest/LandingPageGuest.jsx
+++ frontend/src/pages/guest/LandingPageGuest.jsx
@@ -3,13 +3,14 @@ import SidebarGuest from "../../components/navbar/SidebarGuest";
 import AboutUs from "../../components/Home/AboutUs";
 import { Link, animateScroll as scroll } from "react-scroll";
 
-const LandingPageGuest = ()=>{
-return(
-    <div class="flex flex-row w-full">
-  <div class="basis-1/4"><SidebarGuest/></div>
-  <div class="basis-3/4"><AboutUs/></div>
-</div>
+const LandingPageGuest = () => {
+  return (
+    <div class="flex flex-row w-full ">
+      <div class="basis-1/7"><SidebarGuest /></div>
+      <div class="basis-6/7"><AboutUs /></div>
 
-)
+    </div>
+
+  )
 }
 export default LandingPageGuest
